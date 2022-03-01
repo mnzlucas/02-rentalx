c17a37f5f7cb5bc3cca29dba6bc7576743bcd1c5
mnzlucas <mnzlucas7@gmail.com>
2022-03-01 15:16:59 -0300

    refactoring specifications

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from 'tsyringe';
+
 import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
 
 interface IRequest {
@@ -5,8 +7,12 @@ interface IRequest {
   description: string;
 }
 
+@injectable()
 class CreateSpecificationUseCase {
-  constructor(private SpecificationsRepository: ISpecificationsRepository) {}
+  constructor(
+    @inject('SpecificationsRepository')
+    private SpecificationsRepository: ISpecificationsRepository,
+  ) {}
 
   execute({ name, description }: IRequest): void {
     const specificationAlreadyExists =
